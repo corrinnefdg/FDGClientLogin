2cf52905334571abc52416cfcb422cad130e5ae7
Corrinne Schultze <cs@fdgcreative.com>
2014-03-12 15:59:21 -0400

    separating the get client function and styles

--- index.php
+++ index.php
@@ -16,8 +16,7 @@
 	<!-- header -->
 	<?php include('header.php'); ?>
     
-    
-    <main>
+   	<main>
     
     	<!-- Add new client -->
        <div class="newClientName slideTitle">
@@ -27,45 +26,24 @@
        <section class="newClientInfo slideInfo">
        		
             <form action="insert.php" method="post">
-            	Client Name: <input type="text" name="inputName">
-            	Type: <input type="text" name="inputType">
-              	URL: <input type="text" name="inputUrl">
-              <br />
-              	Username: <input type="text" name="inputUsername">
-              	Password: <input type="text" name="inputPassword">
-              <input type="submit" name="saveButton" value="Save">
+                Client Name: <input type="text" name="inputName">
+                Type: <input type="text" name="inputType">
+                URL: <input type="text" name="inputUrl">
+                <br />
+                Username: <input type="text" name="inputUsername">
+                Password: <input type="text" name="inputPassword">
+                <input type="submit" name="saveButton" value="Save">
             </form>
             
        </section>
-    
-    
-    	<?php
-			
-			include('config.php');
-			
-			echo "<h1>Client:</h1><br />";	
-			
-			// Select statement
-			$select = "SELECT * FROM test ORDER BY client_name";
-				echo $select; 
-			
-			// Display
-			$q_result = $con->query($select) or die('Query did not work.');
-				echo $q_result;
-				
-			// Seeing if there are any matches
-			$anymatches = mysqli_num_rows($q_result);
-				echo $anymatches;
-			
-			
-			// Tryin' to loop through results and display
-			while($result = mysqli_fetch_array( $q_result )){
-				echo "<p><strong>".$result['client_name']."</strong></p>";
-			}
+       
+       
+    	<!-- Temporary "get clients" button -->
+    	<form action="getclients.php" method="post">
+       		<input type="submit" name="getClients" value="Get Clients">
+       </form>
 
-			echo "words";
-		
-		?>
+
     	<!-- Client List -->	
         <div class="clientName slideTitle">
         	<h1>Client Name</h1>
