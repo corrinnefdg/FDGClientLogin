cae4084e29b6a875a253b03c0c5a3e12b73fc346
Corrinne Schultze <cs@fdgcreative.com>
2014-04-07 09:35:43 -0400

    Separating search function into different file

--- search.php
+++ search.php
@@ -1,7 +1,6 @@
 <?php
+	//SEARCH FUNCTION ===============================================
 
-	
-	
 	if (isset($_POST['find'])) {
 		
 		$find = $_POST['find'];
@@ -34,7 +33,8 @@
 			echo "<p>Sorry, but we can not find an entry to match your query.</p>";
 		}
 		else {
-			echo "<p>Found " .$anymatches. " matches for " .$find. ".</p>";
+			// echo "<p>Found " .$anymatches. " matches for " .$find. ".</p>"; 
+			// fix this for client_name
 		}
 	}
 	else
@@ -44,15 +44,57 @@
 	}
 	
 	//And we display the results
-	while($result = mysqli_fetch_array( $query_result ))
-	{
-		echo "<ul>";
-		echo "<li><strong>".$result['client_name']."</strong></li>";
-		echo "<li>".$result['type']."</li>";
-		echo "<li>".$result['url']."</li>";
-		echo "<li>".$result['username']."</li>";
-		echo "<li>".$result['password']."</li>";
-		echo "</ul>";
-	}
+	$client_list = array();
+		
+	// Looping through results to display each client
+	while($result = mysqli_fetch_array( $query_result )){
 
+		//fill the $client_list array with client information
+		$client_name = $result['client_name'];
+		$client_list[$client_name][] = $result;
+	}
+		// get each heading
+		foreach($client_list as $client_name => $client_records){
 ?>
+	 
+<!-- Displaying Results ======================== -->  
+<div class="clientName slideTitle">
+	<h1><?php echo $client_name; ?></h1>
+</div>
+<section class="clientInfo slideInfo">
+
+<?php
+	// get each entry
+	foreach($client_records as $client_record){
+?>
+	<div class="clientEntry">
+	  <ul>
+		<li>Type:<p><?php echo $client_record['type']; ?></p></li>
+		<li>URL:<p><?php echo $client_record['url']; ?></p></li>
+		<li>Username:<p><?php echo $client_record['username']; ?></p></li>
+		<li>Password:<p><?php echo $client_record['password']; ?></p></li>
+		<li>
+				<form action="delete.php" method="post">
+					<input type="hidden" name="clientRecordId" value="<?php echo $cid ?>" />
+					<input type='submit' name='delete' value='Delete Entry' />
+				</form>
+		  </li>
+	  </ul>
+   </div>
+   
+   <?php } ?> <!-- end PHP foreach loop creating each Client Entry -->
+   
+	 <p class="addNew slideNew">Add New Entry</p>
+		<div class="addNewInfo">
+			<form action="insert.php" method="post">
+				Client Name: <input type="text" name="inputName">
+				Type: <input type="text" name="inputType">
+				URL: <input type="text" name="inputUrl">
+				<br />
+				Username: <input type="text" name="inputUsername">
+				Password: <input type="text" name="inputPassword">
+				<input type="submit" name="saveButton" value="Save">
+			</form>
+		</div>
+</section>
+<?php } ?> <!-- end PHP foreach loop creating each Client Name section -->
